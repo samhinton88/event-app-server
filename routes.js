37e8ed92dd68fc09a631d5e28245832c71ac1a97
sam hinton <sam.hinton88@gmail.com>
2018-07-07 10:00:36 +0100

    event routes

--- routes.js
+++ routes.js
@@ -2,4 +2,10 @@ const { userController, eventController } = require('./src/controllers');
 
 module.exports = app => {
   app.post('/api/users/', userController.createUser)
+
+
+  app.get('/api/events', eventController.fetchEvents)
+  app.post('/api/events', eventController.createEvent)
+  app.put('/api/events/:eventId', eventController.editEvent)
+  app.delete('/api/events/:eventId', eventController.deleteEvent)
 };

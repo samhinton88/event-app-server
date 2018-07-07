2dc329c228150662f13764542d4954cbf626b8eb
sam hinton <sam.hinton88@gmail.com>
2018-07-07 17:06:30 +0100

    change image data type to buffer string

--- src/models/event/index.js
+++ src/models/event/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const eventSchema = new Schema({
-  image: String,
+  image: { type: Buffer, contentType: String},
   title: String,
   venue: String,
   time: Date,

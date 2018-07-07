45dcee124ccd8ee0b7d718ac28c024b715f0feba
sam hinton <sam.hinton88@gmail.com>
2018-07-07 12:22:43 +0100

    add leading slash to routes

--- routes.js
+++ routes.js
@@ -4,8 +4,8 @@ module.exports = app => {
   app.post('api/users/', userController.createUser)
 
 
-  app.get('api/events', eventController.fetchEvents)
-  app.post('api/events', eventController.createEvent)
-  app.put('api/events/:eventId', eventController.editEvent)
-  app.delete('api/events/:eventId', eventController.deleteEvent)
+  app.get('/api/events', eventController.fetchEvents)
+  app.post('/api/events', eventController.createEvent)
+  app.put('/api/events/:eventId', eventController.editEvent)
+  app.delete('/api/events/:eventId', eventController.deleteEvent)
 };

2f74aa465d7fb9ce06f5ea21c15803fee8c03640
sam hinton <sam.hinton88@gmail.com>
2018-07-10 10:50:21 +0100

    replace buffer image with cloudinary

--- src/models/event/index.js
+++ src/models/event/index.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const eventSchema = new Schema({
-  image: { data: Buffer, contentType: String},
   title: String,
   venue: String,
   time: Date,
   text: String,
-  interestedUsers: [{type: Schema.Types.ObjectId, ref: 'user'}]
+  interestedUsers: [{type: Schema.Types.ObjectId, ref: 'user'}],
+  cloudinaryUrl: String
 })
 
 module.exports = mongoose.model('event',eventSchema);
